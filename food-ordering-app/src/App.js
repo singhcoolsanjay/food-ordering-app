72bf9b107d24541ded4637845d144ab4927e83c3
Sanjay Singh <35537122+singhcoolsanjay@users.noreply.github.com>
2019-06-20 08:58:10 +0530

    2nd commit

--- food-ordering-app/src/App.js
+++ food-ordering-app/src/App.js
@@ -3,13 +3,20 @@ import Home from './screens/home/Home.js';
 import Details from './screens/details/Details.js';
 import Checkout from './screens/checkout/Checkout.js';
 import Header from './common/header/Header.js';
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
 
 class App extends Component {
     render(){
         return(
-            <h1>
-                App
-            </h1>
+            <Router>
+                <div>
+                <Header/>
+                <Route exact path="/" component={Home}/>
+                <Route path="/details" component={Details}/>
+                <Route path="/checkout" component={Checkout}/>
+                </div>
+            </Router>
         )
     }
 }
--- food-ordering-app/src/common/header/Header.js
+++ food-ordering-app/src/common/header/Header.js
@@ -1,10 +1,19 @@
 import React, {Component} from 'react';
+import { Link } from "react-router-dom";
+import './Header.css';
+import Logo from '../../assets/logo.png';
+
 class Header extends Component {
     render(){
         return(
-            <h1>
-                Header
-            </h1>
+            <div>
+             <img className="Logo" src={Logo} alt="logo"/>
+                <ul className="menu-ul">
+                    <li><Link to="/">Home</Link></li>
+                    <li><Link to="/details">Details</Link></li>
+                    <li><Link to="/checkout">Checkout</Link></li>                  
+                </ul>
+            </div>    
         )
     }
 }
