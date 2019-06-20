4030f7d107072fdad1d3afd5bad24a115e896719
Sanjay Singh <35537122+singhcoolsanjay@users.noreply.github.com>
2019-06-20 08:34:30 +0530

    commit

---
+++ food-ordering-app/src/App.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+import Home from './screens/home/Home.js';
+import Details from './screens/details/Details.js';
+import Checkout from './screens/checkout/Checkout.js';
+import Header from './common/header/Header.js';
+
+class App extends Component {
+    render(){
+        return(
+            <h1>
+                App
+            </h1>
+        )
+    }
+}
+
+export default App;
--- food-ordering-app/src/index.js
+++ food-ordering-app/src/index.js
@@ -2,12 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import * as serviceWorker from './serviceWorker';
+import App from './App.js';
 
-ReactDOM.render(
-    <div>
-        Food Ordering App
-    </div>,
-    document.getElementById('root')
+ReactDOM.render(<App/>,  document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
